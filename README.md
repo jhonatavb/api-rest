142b00e6c0f23fa21aa0d81ee931a20abb4db036
Jhonata Braga <jhonatavbrg@gmail.com>
2022-08-15 20:56:02 -0300

    add dotenv for username and password autentication

---
+++ README.md
@@ -0,0 +1,4 @@
+# Project API REST
+
+A simple but very fun project, it involves a lot of knowledge of the express framework.
+In it I use express to create an API where you can create a product by sending its name and price, the ID is created dynamically. This is all saved in cache and you can consult both the product by ID and allregistered products. The POST, PUT DELETE me and thods are secured by authentication.
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,11 +1,12 @@
 const express = require('express');
+require('dotenv').config();
 
 const auth = (req, res, next) => {
   if(!req.headers.username && !req.headers.password) {
     return res.status(401).send({ message: 'unauthorized user for this operation.' });
   }
 
-  if(req.headers.username !== 'admin' || req.headers.password !== 'admin') {
+  if(req.headers.username !== process.env.DB_USERNAME || req.headers.password !== process.env.DB_PASSWORD) {
     return res.status(401).send({ message: 'username or password is invalid.' });
   }
 
