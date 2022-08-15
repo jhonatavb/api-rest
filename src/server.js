9e2380ca1112c9ce8b1be9793bf8518c2af5a83b
Jhonata Braga <jhonatavbrg@gmail.com>
2022-08-15 13:42:52 -0300

    add method GET in API

--- src/server.js
+++ src/server.js
@@ -3,3 +3,11 @@ const express = require('express');
 const app = express();
 
 const PORT = 3000;
+
+app.get((req, res, next) => {
+  res.send({ nome: 'MacBook PRO M1', preco: 2000.00 });
+});
+
+app.listen(PORT, () => {
+  console.log(`running on port: ${PORT}`);
+});
